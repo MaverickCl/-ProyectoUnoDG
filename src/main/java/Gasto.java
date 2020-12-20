aaf89a75d75e145d2cc328010480df8d7047d501
Diego Garrido 1 <dgarrido@uno.com>
2020-12-20 20:35:37 -0300

    Implementando Gastos

--- src/main/java/Gasto.java
+++ src/main/java/Gasto.java
@@ -1,2 +1,30 @@
 public class Gasto {
+    String nombreGasto;
+    int valor;
+    Boolean pagado;S
+    public Gasto() {
+
+    }
+
+    public Gasto(String nombreGasto, int valor, boolean pagado) {
+        valor = valor;
+        nombreGasto=nombreGasto;
+        pagado=pagado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setNombreGasto(String nombreGasto) {
+        this.nombreGasto = nombreGasto;
+    }
+
+    public String getNombreGasto() {
+        return nombreGasto;
+    }
 }
